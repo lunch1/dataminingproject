f9ad4318046d052297bc606cce5eacb6eaf82a42
Jingjooz <54562554+Jingjooz@users.noreply.github.com>
2020-04-11 09:23:40 -0400

    Update datamining_project.py

--- datamining_project.py
+++ datamining_project.py
@@ -56,40 +56,77 @@ import pandas as pd
 # You can change it with chdir()
 dirpath = os.getcwd() # print("current directory is : " + dirpath)
 filepath = os.path.join( dirpath ,'cepr_org_2019.csv')
-cols_list=['age','female','wbho','forborn','citizen','vet','married','ownchild','empl','unem','nilf','uncov','state','educ','centcity','suburb','rural','smsastat14','ind_m03','agric','manuf','servs','hourslw','rw', 'faminc', 'multjobn', 'wage3']
-df= pd.read_csv(filepath, usecols=cols_list)  
-
+cols_list=['age','female','wbho','forborn','citizen','vet','married', 'marstat','ownchild','empl','unem','nilf','uncov','state','educ','centcity','suburb','rural','smsastat14','ind_m03','agric','manuf', 'hourslw','rw', 'multjobn']
+df= pd.read_csv(filepath, usecols=cols_list)
 dfChkBasics(df, True)
 print(df.dtypes)
 
 # Data dict
-# age - age of US worker
+# age - age (Numeric)
 # female - sex (0 = male, 1 = female)
-# wbho - Race
-# forborn - Foreign born
-# citizen - US citizen
-# vet - Veteran
-# married - Married
-# ownchild - Number of children
-# empl - Employed
-# unem - Unemployed
-# nilf - Not in labor force
-# uncov - Union coverage
-# state - state
-# educ - Education level
-# centcity - Central city
-# suburb - suburbs
-# rural - rural
+# wbho - Race (white, Hispanic, Black, Other)
+# forborn - Foreign born (0 = foriegn born, 1 = US born)
+# citizen - US citizen (0 = No-US citizen, 1 = US citizen)
+# vet - Veteran (0 = No-vateren, 1 = veteran)
+# married - Married (0 = Never married, 1 = married)
+# marstat - Marital status (Married, Never Married, Divorced, Widowed, Separated)
+# ownchild - Number of children (Numeric)
+# empl - Employed (0 = employed, 1 = unemployed)
+# unem - Unemployed (0 = employed, 1 = unemployed)
+# nilf - Not in labor force (0 = Not in labor force, 1 = in labor force)
+# uncov - Union coverage (0 - non-Union coverage, 1 = Union coverage)
+# state - state (50 states)
+# educ - Education level (HS, Some college, College, Advanced, LTHS)
+# centcity - Central city (0 = no Central city, 1 = Central city)
+# suburb - suburbs (0 = no suburbs area, 1 = suburbs area)
+# rural - rural (0 = no rural area, 1 = rural area)
 # smsastat14 - Metro CBSA FIPS Code
-# ind_m03 - Major Industry Recode
-# agric - Agriculture
-# manuf - Manufacturing
-# servs - Services
-# hourslw - Hours last week, all jobs
-# rw - Real wage, 2019$
-# multjobn - Number of jobs
-# wage3 - Hourly wage (both hourly and non-hourly workers)
-# faminc - Family income band
+# ind_m03 - Major Industry Recode (Educational and health services, Wholesale and retail trade, Professional and business services, Manufacturing, Leisure and hospitality, Construction, Financial activities, Other  services, Transportation and utilities, Public administration, Agriculture, forestry, fishing, and hunting, Information, Mining, and Armed Forces)
+# agric - Agriculture (0 = Non-Argiculture job, 1 = Non-Agriculture job)
+# manuf - Manufacturing (0 = Non-Manufacturing job, 1 = Non-Manufacturing job)
+# hourslw - Hours last week, all jobs (Numeric)
+# rw - Real hourly wage, 2019$ (Numeric)
+# multjobn - Number of jobs (Numeric)
 
+
+# %%
+# clean numuric data (3 variables)
+# Step 1 - plot the boxplot to investigate the outlier in "age", "rw", and "houtslw" (Numuric)
+import matplotlib.pyplot as plt 
+import numpy as np
+boxplot = df.boxplot(column=['age', 'rw', 'hourslw'])
+dfnew = df[['age', 'rw', 'hourslw']]
+df.drop(columns=['age', 'rw', 'hourslw'])
+
+# According to the plot, there are a little bit outlier in age. In addition, rw and hourslw have so many outlier, so we need to remove them out. 
+
+#%%
+#Step 2 - remove outlier by calculated the interquartile range (IQR). 
+# IQR is calculated as the difference between the 75th and 25th percentiles or IQR = Q3 − Q1. 
+# So, the numbers that are out of IQR are outlier. 
+Q1 = dfnew.quantile(0.25)
+Q3 = dfnew.quantile(0.75)
+IQR = Q3 - Q1
+print(IQR)
+
+df_new = dfnew[~((dfnew < (Q1 - 1.5 * IQR)) |(dfnew > (Q3 + 1.5 * IQR))).any(axis=1)]
+print(df_new.shape)
+
+
+# %%
+# Step 3
+# merge the data (3 numuric) without outlier to original dataset by index of both the dataframes.
+cleaned_df = df.merge(df_new, left_index=True, right_index=True)
+
+# reset index for new dataset
+cleaned_df = cleaned_df.reset_index()
+
+#rename age_x, rw_x, hourlw_x to age, rw, and hourlw, and drop age_y, rw_y, and hourlw_y
+cleaned_df = cleaned_df.rename(columns={"age_x": "age", "rw_x": "rw", "hourslw_x": "hourslw"})
+cleaned_df = cleaned_df.drop(columns=['age_y', 'rw_y', 'hourslw_y','index'])
+
+#Cleaned_data
+dfChkBasics(cleaned_df, True)
+print(cleaned_df.dtypes)
 
 # %%
