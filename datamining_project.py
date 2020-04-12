0bf46cfb37ea74229b904193175719a5852f8c6d
Jingjooz <54562554+Jingjooz@users.noreply.github.com>
2020-04-12 02:47:42 -0400

    Update datamining_project.py

--- datamining_project.py
+++ datamining_project.py
@@ -187,4 +187,84 @@ cleaned_df.rural.sum()
 data = [['Central City', 2297], ['Suburban', 4385], ['Rural', 1893]]
 regions = pd.DataFrame(data, columns = ['Type of Region', 'Count']) 
 regions_ch=sns.barplot(x='Type of Region',y='Count',data=regions,palette='Greens')
+
+
 # %%
+# Build the model
+# Mutiple regression
+
+# age - age (Numeric)
+# female - sex (0 = male, 1 = female)
+# wbho - Race (white, Hispanic, Black, Other)
+# forborn - Foreign born (0 = foriegn born, 1 = US born)
+# citizen - US citizen (0 = No-US citizen, 1 = US citizen)
+# vet - Veteran (0 = No-vateren, 1 = veteran)
+# married - Married (0 = Never married, 1 = married)
+# marstat - Marital status (Married, Never Married, Divorced, Widowed, Separated)
+# ownchild - Number of children (Numeric)
+# empl - Employed (0 = employed, 1 = unemployed)
+# unem - Unemployed (0 = employed, 1 = unemployed)
+# nilf - Not in labor force (0 = Not in labor force, 1 = in labor force)
+# uncov - Union coverage (0 - non-Union coverage, 1 = Union coverage)
+# state - state (50 states)
+# educ - Education level (HS, Some college, College, Advanced, LTHS)
+# centcity - Central city (0 = no Central city, 1 = Central city)
+# suburb - suburbs (0 = no suburbs area, 1 = suburbs area)
+# rural - rural (0 = no rural area, 1 = rural area)
+# smsastat14 - Metro CBSA FIPS Code
+# ind_m03 - Major Industry Recode (Educational and health services, Wholesale and retail trade, Professional and business services, Manufacturing, Leisure and hospitality, Construction, Financial activities, Other  services, Transportation and utilities, Public administration, Agriculture, forestry, fishing, and hunting, Information, Mining, and Armed Forces)
+# agric - Agriculture (0 = Non-Argiculture job, 1 = Non-Agriculture job)
+# manuf - Manufacturing (0 = Non-Manufacturing job, 1 = Non-Manufacturing job)
+# hourslw - Hours last week, all jobs (Numeric)
+# rw - Real hourly wage, 2019$ (Numeric)
+# multjobn - Number of jobs (Numeric)
+
+from statsmodels.formula.api import ols
+modelwage1Fit = ols(formula='rw ~ age + C(female) + hourslw + C(forborn) + C(married) + C(educ) + C(wbho) + ownchild', data=cleaned_df).fit()
+
+print( type(modelwage1Fit) )
+print( modelwage1Fit.summary() )
+
+modelpredicitons = pd.DataFrame( columns=['modelwage1_LM'], data= modelwage1Fit.predict(cleaned_df)) 
+# use the original dataset gpa data to find the expected model values
+print(modelpredicitons.shape)
+print( modelpredicitons.head() )
+
+# All of them were significant.
+
+# Note: citizen and uncov were not significant, so both of them were dropped from the model
+
+#%%
+# Check the VIF value (watch out for multicollinearity issues)
+from statsmodels.stats.outliers_influence import variance_inflation_factor
+
+# need to convert educ and wbho to numeric by function because VIF calculation requires numeric variables.
+# Educ 
+# HS = 0, Some college = 1, college = 2, advanced = 3, LTHS = 4
+def cleanDfeduc(row):
+  theedu = row["educ"]
+  return (0 if (theedu=="HS") else 1 if (theedu=="Some college") else 2 if (theedu=="College") else 3 if (theedu=="Advanced") else 4 if (theedu=="LTHS") else np.nan)
+# end function cleanDfeduc
+cleaned_df['educ'] = df.apply(cleanDfeduc, axis=1)
+
+# wbho
+# White = 0, Hispanic = 1, black = 2, other = 3
+def cleanDfwbho(row):
+  thewbho = row["wbho"]
+  return (0 if (thewbho=="White") else 1 if (thewbho=="Hispanic") else 2 if (thewbho=="Black") else 3 if (thewbho=="Other") else np.nan)
+# end function cleanDfeduc
+cleaned_df['wbho'] = df.apply(cleanDfwbho, axis=1)
+
+# Get variables for which to compute VIF and add intercept term
+X = cleaned_df[['age' , 'female' , 'hourslw', 'forborn', 'married', 'ownchild', 'wbho', 'educ']]
+X['Intercept'] = 1
+
+# Compute and view VIF
+vif = pd.DataFrame()
+vif["variables"] = X.columns
+vif["VIF"] = [ variance_inflation_factor(X.values, i) for i in range(X.shape[1]) ] # list comprehension
+
+# View results using print
+print(vif)
+# According to the results, VIF is very small in all independent variables, so it means there are not multicollinearity issues for this model
+
